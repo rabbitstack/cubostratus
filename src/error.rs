0afd5ffe0964357d7176d161768596d94b62fb11
rabbitstack <bhdildo@gmail.com>
2017-04-02 22:18:45 +0200

    added configuration reading/parsing exceptions

--- src/error.rs
+++ src/error.rs
@@ -6,10 +6,13 @@ use std::fmt;
 pub type Result<T> = result::Result<T, Error>;
 
 /// Possible errors produced by syscall collector.
+#[derive(Deserialize, Debug)]
 pub enum Error {
     RingBufferMapping,
     TooManyCollectors,
-    DeviceError
+    DeviceError,
+    UnknownConfigPathError,
+    ConfigParseError(String)
 }
 
 impl fmt::Display for Error {
@@ -17,7 +20,12 @@ impl fmt::Display for Error {
         match *self {
             Error::RingBufferMapping => write!(f, "Unable to map ring buffer device"),
             Error::TooManyCollectors => write!(f, "Too many collectors attached to device"),
-            Error::DeviceError => write!(f, "Insufficient privileges to open device or device not loaded")
+            Error::DeviceError => write!(f, "Insufficient privileges to open device \
+                                        or device not loaded"),
+            Error::UnknownConfigPathError => write!(f, "Unable to resolve \
+                                                configuration file path"),
+            Error::ConfigParseError(ref e) => write!(f, "Invalid configuration descriptor. \
+                                               Reason: {}", e)
         }
     }
 }
